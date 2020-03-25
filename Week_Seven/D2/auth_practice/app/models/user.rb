a3a20b4f79ff3f1b1606363b04a87e3ebce8ecc1
Shaphen <swpburn@gmail.com>
2020-03-24 17:02:34 -0700

    Changes to auth_practice

--- Week_Seven/D2/auth_practice/app/models/user.rb
+++ Week_Seven/D2/auth_practice/app/models/user.rb
@@ -1,25 +1,20 @@
 class User < ApplicationRecord
-    # validates :username, :password, :session_token, presence: true
-    # validates :password, length: {minimum: 4, allow_nil: true}
-    # before_validation :ensure_session_token
-    validates :username, presence: true, uniqueness: true
-    validates :password, presence: { message: 'Password can\'t be blank'}
-    validates :session_token, presence: true, uniqueness: true
-    validates :pass, length: { minimum: 6, allow_nil: true }
+    validates :username, :password, :session_token, presence: true
+    validates :password, length: {minimum: 4, allow_nil: true}
     before_validation :ensure_session_token
 
     attr_reader :pass
     
-    def self.find_by_credentials(username, pass)
-        user = User.find_by(username: username)
-        correct_pass = BCrypt::Password.new(user.password)
-        return user if user && correct_pass.is_password?(pass)
-        nil
-    end
+    # def self.find_by_credentials(username, pass)
+    #     user = User.find_by(username: username)
+    #     correct_pass = BCrypt::Password.new(user.password)
+    #     return user if user && correct_pass.is_password?(pass)
+    #     nil
+    # end
 
-    def self.generate_session_token
-        SecureRandom::urlsafe_base64(16)
-    end
+    # def self.generate_session_token
+    #     SecureRandom::urlsafe_base64(16)
+    # end
 
     def reset_session_token
         self.session_token = User.generate_session_token
@@ -27,9 +22,7 @@ class User < ApplicationRecord
     end
 
     def ensure_session_token
-        unless self.session_token
-            self.session_token = User.generate_session_token
-        end
+        self.session_token ||= User.generate_session_token
     end
 
     def password=(password)
